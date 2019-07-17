80d4f4520840194694b47dbdc26b040eb8da6f4f
Rick West <rickwestdev@gmail.com>
2019-07-17 12:04:22 +0100

    Further development and formatting with Black

--- __init__.py
+++ __init__.py
@@ -3,84 +3,109 @@
 """Search the Laravel Documentation"""
 
 from os import path
+import urllib.parse
+import html
 from algoliasearch.search_client import SearchClient
 
 from albertv0 import *
 
-__iid__ = 'PythonInterface/v0.2'
-__prettyname__ = 'Laravel Docs'
-__version__ = '0.1'
-__trigger__ = 'ld '
-__author__ = 'Rick West'
-__dependencies__ = ['algoliasearch']
+__iid__ = "PythonInterface/v0.2"
+__prettyname__ = "Laravel Docs"
+__version__ = "0.1"
+__trigger__ = "ld "
+__author__ = "Rick West"
+__dependencies__ = ["algoliasearch"]
 
 
-client = SearchClient.create('8BB87I11DE', '8e1d446d61fce359f69cd7c8b86a50de')
-index = client.init_index('docs')
+client = SearchClient.create("8BB87I11DE", "8e1d446d61fce359f69cd7c8b86a50de")
+index = client.init_index("docs")
 
 
-icon = '{}/icon.png'.format(path.dirname(__file__))
+icon = "{}/icon.png".format(path.dirname(__file__))
 
 
 def getSubtitle(hit):
-    if hit['h4'] is not None:
-        return hit['h4']
+    if hit["h4"] is not None:
+        return hit["h4"]
 
-    if hit['h3'] is not None:
-        return hit['h3']
+    if hit["h3"] is not None:
+        return hit["h3"]
 
-    if hit['h2'] is not None:
-        return hit['h2']
+    if hit["h2"] is not None:
+        return hit["h2"]
 
-    return ''
+    return None
 
 
 def handleQuery(query):
-    results = []
+    items = []
 
     if query.isTriggered and query.string.strip():
 
         if not query.isValid:
             return
 
-        item = Item(
-            id=__prettyname__,
-            icon=icon,
-            completion=query.rawString,
-            text=__prettyname__,
-            actions=[]
-        )
+        search = index.search(query.string, {"tagFilters": "master"})
 
-        if len(query.string) >= 4:
-            search = index.search(query, {'tagFilters': 'master'})
+        urls = []
+        docs = "https://laravel.com/docs/"
 
-            urls = []
-            docs = 'https://laravel.com/docs/'
+        for hit in search["hits"]:
+            url = "{}{}".format(docs, hit["link"])
 
-            for hit in search['hits']:
-                url = '{}{}'.format(docs, hit['link'])
+            if url in urls:
+                continue
 
-                if url in urls:
-                    continue
+            urls.append(url)
 
-                urls.append(url)
+            title = hit["h1"]
+            subtitle = getSubtitle(hit)
 
-                title = hit['h1']
-                subtitle = getSubtitle(hit)
+            text = False
+            try:
+                text = hit["_highlightResult"]["content"]["value"]
+            except KeyError:
+                pass
 
-                results.append(Item(
+            if text and subtitle:
+                title = "{} - {}".format(title, subtitle)
+                subtitle = text
+
+            items.append(
+                Item(
                     id=__prettyname__,
                     icon=icon,
-                    text=title,
-                    subtext=subtitle,
-                    actions=[UrlAction('Open in the Laravel Documentation', url)])
+                    text=html.unescape(title),
+                    subtext=html.unescape(subtitle),
+                    actions=[UrlAction("Open in the Laravel Documentation", url)],
                 )
+            )
 
-            if len(results) == 0:
-                item.subtext = 'No results found :('
-                return item
+        if len(items) == 0:
 
-        else:
-            item.subtext = 'Search the Laravel Documentation!'
-            return item
-    return results
+            google = "https://www.google.com/search?q={}".format(
+                urllib.parse.quote("laravel {}".format(query.string))
+            )
+
+            items.append(
+                Item(
+                    id=__prettyname__,
+                    icon=icon,
+                    text="Search Google",
+                    subtext='No match found. Search Google for: "{}"'.format(
+                        query.string
+                    ),
+                    actions=[UrlAction("No match found. Search Google", google)],
+                )
+            )
+            items.append(
+                Item(
+                    id=__prettyname__,
+                    icon=icon,
+                    text="Open Docs",
+                    subtext="No match found. Open laravel.com/docs...",
+                    actions=[UrlAction("Open the Laravel Documentation", docs)],
+                )
+            )
+
+    return items
